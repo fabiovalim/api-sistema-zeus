4b53229ce000d7d4f14be3bf9231e149bcb9d0b0
Fabio Valim <oficialvalim@gmail.com>
2025-05-13 16:41:05 -0300

    feat: create budgets models

---
+++ src/apps/models/Budgets.js
@@ -0,0 +1,29 @@
+const Sequelize = require('sequelize');
+const { Model } = require('sequelize');
+
+class Budgets extends Model {
+    static init (sequelize) {
+        super.init (
+            {
+                name: Sequelize.STRING,
+                description: Sequelize.TEXT,
+                user_id: Sequelize.INTEGER,
+                client: Sequelize.STRING,
+                estimated_value: Sequelize.DECIMAL(10, 2),
+                predicted_cost: Sequelize.DECIMAL(10, 2),
+                status: Sequelize.STRING,
+            },
+            {
+                sequelize
+            }
+        );
+        
+        return this; 
+    }
+
+    static associate(models) {
+        this.belongTo(models.Users, {foreignKey: 'user_id', as: 'user'});
+    }
+}
+
+module.exports = Budgets;
