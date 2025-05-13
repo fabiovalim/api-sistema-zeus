a5e866d319233d84991722dbc186d297dc266c13
Fabio Valim <oficialvalim@gmail.com>
2025-05-13 16:34:27 -0300

    feat: create users models

--- src/apps/models/Users.js
+++ src/apps/models/Users.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 const { Model } = require('sequelize');
-// const bcryptjs = require('bcryptjs');
+const bcryptjs = require('bcryptjs');
 
 class Users extends Model {
     static init(sequelize) {
@@ -8,31 +8,30 @@ class Users extends Model {
             {
                 name: Sequelize.STRING,
                 email: Sequelize.STRING,
-                entry_date: Sequelize.DATE,
                 image: Sequelize.STRING,
                 position: Sequelize.STRING,
-                gender: Sequelize.STRING,
-                phone: Sequelize.STRING,
-                skill: Sequelize.STRING,
-                entry_date: Sequelize.DATE
+                phone:  Sequelize.STRING,
+                skill:  Sequelize.STRING,
+                password_hash: Sequelize.STRING,
+                password: Sequelize.VIRTUAL
             },  
             {
                 sequelize
             }
         )
 
-        // this.addHook('beforeSave', async (user) => {
-        //     if(user.password) {
-        //         user.password_hash = await bcryptjs.hash(user.password, 8);
-        //     }
-        // });
+        this.addHook('beforeSave', async (user) => {
+            if(user.password) {
+                user.password_hash = await bcryptjs.hash(user.password, 8);
+            }
+        });
 
         return this;
     };
     
-    // checkPassword(password) {
-    //     return bcryptjs.compare(password, this.password_hash);
-    // };
+    checkPassword(password) {
+        return bcryptjs.compare(password, this.password_hash);
+    };
 };
 
 module.exports = Users;
