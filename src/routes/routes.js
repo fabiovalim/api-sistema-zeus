987e5e6d6ca5e716b94445ec05a96e11d12db0f1
Fabio Valim <oficialvalim@gmail.com>
2025-05-15 10:40:28 -0300

    feat: add user authentication

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -3,12 +3,16 @@ const UserController = require('../apps/controllers/UserController');
 const BudgetController = require('../apps/controllers/BudgetController');
 const AdminController = require('../apps/controllers/AdminController');
 const schemaValidator = require('../apps/middlewares/schemaValidator');
+const AuthenticationMiddleware = require('../apps/middlewares/authentication');
+const AuthenticationController = require('../apps/controllers/AuthenticationController');
 const UserSchema = require('../schema/create.user.schema.json');
 const BudgetSchema = require('../schema/create.budget.schema.json');
+const AuthSchema = require('../schema/auth.schema.json');
 
 const routes = new Router();
 
 routes.get('/health', (req, res) => {return res.send({ message: 'Connected...' })});
+routes.post('/auth', schemaValidator(AuthSchema), AuthenticationController.authenticate);
 
 routes.get('/user/:id', UserController.getUser);
 routes.post('/user', schemaValidator(UserSchema), UserController.create);
