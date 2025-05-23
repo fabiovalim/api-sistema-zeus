493f0d1ff5dbd5bb42c6d2664cac5a5fc6c35e03
Fabio Valim <oficialvalim@gmail.com>
2025-05-22 22:55:42 -0300

    feat: add route upload image

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,13 +1,14 @@
-// centralizar todas as rotas 
-
 const { Router } = require('express');
 const AuthenticationMiddleware = require('../apps/middlewares/authentication');
+
+// centralizar todas as rotas 
 
 const authRoutes = require('./auth.routes');
 const userRoutes = require('./user.routes');
 const budgetRoutes = require('./budget.routes');
 const penaltyRoutes = require('./penalty.routes');
 const adminRoutes = require('./admin.routes');
+const uploadRoutes = require('./upload.routes');
 
 const routes = Router();
 
@@ -17,8 +18,9 @@ routes.use(authRoutes);
 routes.use(AuthenticationMiddleware); // rotas abaixo são protegidas com autenticação
 
 routes.use(userRoutes);
+routes.use(adminRoutes);
 routes.use(budgetRoutes);
 routes.use(penaltyRoutes);
-routes.use(adminRoutes);
+routes.use(uploadRoutes);
 
 module.exports = routes;
